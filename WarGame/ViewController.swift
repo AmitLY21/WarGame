b2b8496cfd8dd4d59dce0518b75b4a3ab31e124a
AmitLY21 <amitly18@gmail.com>
2022-04-12 15:27:12 +0200

    Initial Commit

---
+++ WarGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarGame
+//
+//  Created by user216694 on 4/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WarGameUITests/WarGameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WarGameUITestsLaunchTests.swift
+//  WarGameUITests
+//
+//  Created by user216694 on 4/12/22.
+//
+
+import XCTest
+
+class WarGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
